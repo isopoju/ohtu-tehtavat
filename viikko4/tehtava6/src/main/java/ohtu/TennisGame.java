e9d346bd547147e3bb943bfbcc304abf59e1d9a5
Juha Isopoussu <isopoju@gmail.com>
2020-04-30 16:03:44 +0300

    part4 task6

--- viikko4/tehtava6/src/main/java/ohtu/TennisGame.java
+++ viikko4/tehtava6/src/main/java/ohtu/TennisGame.java
@@ -2,79 +2,64 @@ package ohtu;
 
 public class TennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private int playerOneScore = 0;
+    private int playerTwoScore = 0;
+    private String playerOneName;
+    private String playerTwoName;
 
-    public TennisGame(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
+    public TennisGame(String playerOneName, String playerTwoName) {
+        this.playerOneName = playerOneName;
+        this.playerTwoName = playerTwoName;
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        if (playerName.equals("player1")) {
+            playerOneScore += 1;
+        } else {
+            playerTwoScore += 1;
+        }
+    }
+
+    private boolean atLeastAnotherScoreIsAtLeastFour() {
+        return (playerOneScore >= 4 || playerTwoScore >= 4) ? true : false;
+    }
+    
+    private boolean bothHaveSameScore() {
+        return (playerOneScore == playerTwoScore) ? true : false;
+    }
+   
+    private boolean differenceBetweenScoresIsAtLeastTwo() {
+        return (Math.abs(playerOneScore - playerTwoScore) > 1) ? true : false;
+    }
+
+    private boolean differenceBetweenScoresIsExactlyOne() {
+        return (Math.abs(playerOneScore - playerTwoScore) == 1) ? true : false;
+    }
+
+    private boolean hasAdvantage() {
+        return (atLeastAnotherScoreIsAtLeastFour() && differenceBetweenScoresIsExactlyOne()) ? true : false;
+    }
+    
+    private boolean hasWinner() {
+        return (atLeastAnotherScoreIsAtLeastFour() && differenceBetweenScoresIsAtLeastTwo()) ? true : false;
+    }
+   
+    private String translateScore(int score) {
+        String[] scores = {"Love", "Fifteen", "Thirty", "Forty"};
+	return scores[score];
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                case 3:
-                        score = "Forty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
-        }
-        return score;
+        
+        if (bothHaveSameScore())
+            return (playerOneScore < 4) ? translateScore(playerOneScore) + "-All" : "Deuce";
+        
+        if (hasWinner())
+            return (playerOneScore > playerTwoScore) ? "Win for player1" : "Win for player2";
+        
+        if (hasAdvantage())
+            return (playerOneScore > playerTwoScore) ? "Advantage player1" : "Advantage player2";
+        
+        return translateScore(playerOneScore) + "-" + translateScore(playerTwoScore);
     }
 }
